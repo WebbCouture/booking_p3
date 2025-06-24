3c3d3a3a75408db9657552b606a4a5649b8c854c
WebbCouture <lesnik.amanda@icloud.com>
2025-06-25 00:17:05 +0200

    Set up email sending with Gmail app password

--- booking_p3/settings.py
+++ booking_p3/settings.py
@@ -94,8 +94,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
-# Email (for dev only)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Email (Gmail via app password)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
